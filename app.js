195623e58afec8742e99a000fad9866ccf2caf4a
Suprim Lal Amatya <suprim.amatya53@gmail.com>
2023-09-25 23:12:32 +0545

    add day 2 code express js and node js

--- app.js
+++ app.js
@@ -21,3 +21,33 @@
 
 // console.log("This person's name is" + person.name + " and his age is " + person.age)
 
+// DAY 2 -------------------------------------------------------------------------------------------------
+
+const app = require("express")()
+
+// ALTERNATIVE tala ko duita ekai tali lekhna paryo.. mathi ko alternative
+// const express = require("express")
+// const app = express()
+
+// req-request, res-response
+app.get("/", (req,res)=>{
+    // console.log(req)
+    // res.send("<h1>Hello I'm home page</h1>")
+    res.json({
+        message: "I am from home page"
+    })
+})
+
+app.get("/contact", (req,res)=>{
+    res.send("This is the contact page")
+})
+
+app.get("/about", (req,res)=>{
+    console.log("This is the about page")
+})
+
+// 3000 port ko room chai use gar yo project run garna lai.. aru port ni rakhna paiyo
+// Node ma explicitly(afaile vannu paryo) yo port ma run ho vanera
+app.listen(3000, (req,res)=>{
+    console.log("Nodejs has started at port 3000")
+})
