be101641acf81975aed82d449996291b563069ca
Suprim Lal Amatya <suprim.amatya53@gmail.com>
2023-09-25 23:02:31 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// console.log("hello world")
+// console.log(1+2)
+
+// const a=10;
+// const b=10;
+// const names="Manish"
+// const sum = a+b
+
+// console.log(sum);
+
+// // array
+// const numbers = [10,20,30]
+// console.log(numbers[0])
+// console.log(numbers.length)
+
+// // objects
+// const person = {
+//     name : "Manish",
+//     age : "12"
+// }
+
+// console.log("This person's name is" + person.name + " and his age is " + person.age)
+
